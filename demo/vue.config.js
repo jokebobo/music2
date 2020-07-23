29dc719852e37da8b107d021ec75317c9b71d984
jokerbobo33 <chenjia9393@163.com>
2020-07-23 10:01:24 +0800

    优化了webpack配置

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,6 +1,42 @@
 const baseUrl = 'http://localhost:2500';
+let cdn = {
+  css: [],
+  js: [
+    'https://cdn.bootcss.com/vue/2.5.17/vue.runtime.min.js',
+    'https://cdn.bootcss.com/vue-router/3.0.1/vue-router.min.js',
+    'https://cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
+    'https://cdn.bootcss.com/axios/0.18.0/axios.min.js'
+  ]
+}
 module.exports = {
   publicPath: process.env.NODE_ENV == 'production' ? './' : '',// 因为页面静态文件是通过相对路径访问 为了项目可以部署在任意路径 publicPath需要配置
+  lintOnSave: true,
+  chainWebpack: config => {
+    if (process.env.NODE_ENV == 'production') {
+      // 生产环境下配置
+      // 生产环境注入cdn
+      config.plugin('html')
+        .tap(args => {
+          args[0].cdn = cdn;
+          return args;
+        });
+      // 压缩代码
+      config.optimization.minimize(true)
+    } else {
+
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV == 'production') {
+      // 生产环境下配置
+      config.externals = {
+        'vue': 'Vue',
+        'vuex': 'Vuex',
+        'vue-router': 'VueRouter',
+        'axios': 'axios'
+      }
+    }
+  },
   devServer: {
     port: '8000',
     // 配置代理服务器 解决跨域
